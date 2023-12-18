e302014f1d448a7ad49a0308d95a42c2bae4ce15
thiago-laza <thiago.profmat@gmail.com>
2023-12-17 21:47:02 -0300

    Mac: inicio function ex 101 e 102

--- mundo-3/4_funcoes/ex101.py
+++ mundo-3/4_funcoes/ex101.py
@@ -3,3 +3,21 @@ Crie um programa que tenha uma funcao chamada voto() que vai receber como
 parametro o ano de nascimento de uma pessoa, retornando um valor literal
 indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATORIO nas eleicoes.
 '''
+from datetime import datetime
+def voto(nascimento):
+    idade = ano_atual - nascimento
+    if idade < 16:
+        print(f'NEGADO! Com {idade} anos nao e permitido votar.')
+    elif idade >= 16 and idade <18 or idade > 65:
+        print(f'OPCIONAL! Com {idade} anos o voto e opcional.')
+    else:
+        print(f'OBRIGATORIO! Com {idade} anos o voto e obrigatorio.')
+
+
+
+
+nascimento = int(input('Ano de nascimento: '))
+data_atual = datetime.now()
+ano_atual = data_atual.year
+
+voto(nascimento)
--- mundo-3/4_funcoes/ex102.py
+++ mundo-3/4_funcoes/ex102.py
@@ -10,3 +10,26 @@ print(n) -> resultado
 print(n,show=True) -> n.(n-1) ... resultado
 obs:criar help da function
 '''
+
+def fatorial(num,verifica=False):
+    f = 1
+    for c in range(num,0,-1):
+        f*=c
+    print(f'O fatorial de {num} e igual a {f}.')
+
+    if verifica == True:
+        j = 1
+        for k in range(num,0,-1):
+            j*=k
+            print(k,end='')
+            if k != 1:
+                print('.',end='')
+        print(f'= {f}')
+
+
+num = int(input('numero: '))
+fatorial(num)
+verifica = input(f'Deseja ver o processo de calculo do fatorial de {num}? [s/n]: ')
+if verifica == 's':
+    fatorial(num,verifica=True)
+
