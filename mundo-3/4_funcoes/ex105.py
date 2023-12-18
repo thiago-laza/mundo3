3cdac009c573f157c9fed2294021db21b9622d96
thiago-laza <thiago.profmat@gmail.com>
2023-12-18 06:46:52 -0300

    Home: function ex105 incompleto

--- mundo-3/4_funcoes/ex105.py
+++ mundo-3/4_funcoes/ex105.py
@@ -16,3 +16,28 @@ saida:{'total': 4,'maior':10,'menor':5.5,'media':7.8}
 saidacom
 obs: colocar help
 '''
+def notas(*n,sit=False):
+    cont = 0
+    acu = 0
+    resultado = {}
+    maior_nota = menor_nota = None
+    for c in range(len(n)):
+        acu+=n[c]
+        cont+=1
+        media = acu/cont
+        resultado['total'] = cont
+        if maior_nota is None or n[c] > maior_nota:
+            maior_nota = n[c]
+            resultado['maior'] = maior_nota
+        if menor_nota is None or n[c] < menor_nota:
+            menor_nota = n[c]
+            resultado['menor'] = menor_nota
+        resultado['media'] = media
+        if sit = True and media < 5:
+            resultado['situacao'] = 'Ruim'
+        dicionario = resultado
+    return dicionario
+
+
+resp = notas(6, 5, 7, sit=True)
+print(resp)
