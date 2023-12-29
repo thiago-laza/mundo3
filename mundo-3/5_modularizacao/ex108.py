f3d88a307054e9a40f22cfdf4608f21dbef541d4
thiago-laza <thiago.profmat@gmail.com>
2023-12-29 14:56:38 -0300

    Home modularizacao inicio

--- mundo-3/5_modularizacao/ex108.py
+++ mundo-3/5_modularizacao/ex108.py
@@ -2,3 +2,9 @@
 Adapte o codigo do desafio 107, criando uma funcao adicional chamada moeda()
 que consiga mostar os valores como um valor monetario formatado.
 '''
+from ex108_p import moeda
+p = float(input('Digite o preco: R$ '))
+print(f'A metade de {moeda.moeda(p)} e {moeda.moeda(moeda.metade(p))}')
+print(f'O dobro de {moeda.moeda(p)} e {moeda.moeda(moeda.dobro(p))}')
+print(f'Aumentando 10%, temos {moeda.moeda(moeda.aumentar(p))}')
+print(f'Reduzindo 13%, temos {moeda.moeda(moeda.diminuir(p))}')
---
+++ mundo-3/5_modularizacao/ex108_p/moeda.py
@@ -0,0 +1,18 @@
+def metade(p):
+    return p / 2
+
+
+def dobro(p):
+    return p * 2
+
+
+def aumentar(p):
+    return p * 1.1
+
+
+def diminuir(p):
+    return p * 0.87
+
+
+def moeda(p):
+    return f'R${p:.2f}'
